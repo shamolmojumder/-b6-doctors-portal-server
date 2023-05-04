a2dba605537379f0fed557a55789850082be411c
shamolmojumder <shamolmojumder14@gmail.com>
2023-05-04 10:55:39 +0600

    74-2 Load available options from database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const port =process.env.PORT ||5000
 
 
@@ -9,11 +10,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// doctor_admin
+// EIXGjkkMvVSHKFSo
 
-
-
-const uri = "mongodb+srv://doctor_admin:<password>@cluster0.gnhfc1r.mongodb.net/?retryWrites=true&w=majority";
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gnhfc1r.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -23,20 +24,22 @@ const client = new MongoClient(uri, {
   }
 });
 
+
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    const appoinmentOptionsCollection = client.db("b6doctorsportal").collection("appoinmentOptions");
+
+    app.get("/appoinmentOptions",async(req,res)=>{
+      const query={};
+      const options=await appoinmentOptionsCollection.find(query).toArray();
+      res.send(options)
+    })
+  } 
+  finally{
+    
   }
 }
 run().catch(console.dir);
-
 
 
 app.get('/', async(req, res) => {
